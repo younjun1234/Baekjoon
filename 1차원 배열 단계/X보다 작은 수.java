d22d7a1f510b99c571266287c943269ce802de86
Younjjjuny <91817425+younjun1234@users.noreply.github.com>
2024-03-09 16:51:48 +0900

    Create X보다 작은 수.java

---
+++ 1차원 배열 단계/X보다 작은 수.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		String output = "";
+		
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if (X > num) {
+				output += num + " ";				
+			}
+		}
+		bw.write(output.trim() + "\n");
+		bw.close();
+		
+	}
+}
