d3232c62ac85be61eec02643d11f6a812a4b83f3
Younjjjuny <91817425+younjun1234@users.noreply.github.com>
2024-03-09 16:36:54 +0900

    Create A+B-8.java

---
+++ 반복문/A+B-8.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			bw.write(String.format("Case #%d: %d + %d = %d\n", i, a, b, a + b));
+		}
+		bw.close();
+	}
+}
