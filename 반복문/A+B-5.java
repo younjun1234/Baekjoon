22f2cf17810842e633cdb8dea751b7a895a8658c
Younjjjuny <91817425+younjun1234@users.noreply.github.com>
2024-03-09 16:38:48 +0900

    Create A+B-5.java

---
+++ 반복문/A+B-5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if (a+b == 0) {
+				break;
+			} else {
+				bw.write(a+b + "\n");
+			}
+		}
+		bw.close();
+	}
+}
