fa3cbecc4063799dcf43b792c5d5110bf2333d56
Younjjjuny <91817425+younjun1234@users.noreply.github.com>
2024-03-12 16:22:33 +0900

    Create 진법 전환 2.java

---
+++ 일반 수학 1/진법 전환 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String code = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		int n = Integer.parseInt(input.split(" ")[0]);
+		int b = Integer.parseInt(input.split(" ")[1]);
+		
+		String answer = "";
+		while(n > 0) {
+			int mod = n % b;
+			n /= b;
+			answer = code.charAt(mod) + answer;
+		}
+		System.out.println(answer);
+	}
+	
+	
+}
