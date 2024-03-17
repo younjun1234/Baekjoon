252aa287cf5594e68e5dc35b50ad98ba7b26e57d
Younjjjuny <91817425+younjun1234@users.noreply.github.com>
2024-03-17 15:10:44 +0900

    Create 좌표 정렬하기.java

---
+++ 정렬/좌표 정렬하기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		Integer[][] arr = new Integer[n][2];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+
+    // quick sort using lambda expression
+		Arrays.sort(arr, (n1, n2) -> {
+			if(n1[0].equals(n2[0])) {
+				return n1[1] - n2[1];
+			} else {
+				return n1[0] - n2[0];
+			}
+		});
+		
+		for(int i=0; i<n; i++) {
+			sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		
+	}
+
+}
